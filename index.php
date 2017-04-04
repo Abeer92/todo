6e73c65bdea4e2c4b8130d83151919f5ad4ad2e0
Abeer92 <abeer_alwakeel@yahoo.com>
2017-04-04 20:51:58 +0200

    updating index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!-- here we are telling the browser that we are using the latest virsion of html-->
 <html>
 <head>
-	<title>Assignment 4</title>
+	<title> <?php echo'Assignment 4'?> </title>
 	
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
@@ -12,7 +12,7 @@
 	<h1 id="heading1">To-do List</h1>
 	<form name="myForm">
 		<input type="text" name="myText" placeholder="please type your text"/>
-		<button id="myBtn"><?php echo 'Enter Your List Item' ?></button>
+		<button id="myBtn"> <?php echo 'Enter Your List Item' ?> </button>
 		<ul class="myList">
 
 		</ul>
